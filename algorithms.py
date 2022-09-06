b5a307292e05181f783a68a32146f2d46e6b6a3e
WallochMatt <matthewrwalloch@gmail.com>
2022-09-06 11:04:54 -0500

    feat: Finished the first task

--- algorithms.py
+++ algorithms.py
@@ -1,1 +1,15 @@
 #Even or Odd
+
+
+#Constant O(1)
+def is_it_even(number):
+    """
+    This function takes in an integer as a parameter
+    The print statement is for display, place any integer in as an argument
+    """
+    if (number % 2) == 0:
+        return True
+    else:
+        return False
+
+print(is_it_even())
