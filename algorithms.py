09d3a3a669b4525c662b238b0df2668d904a9b34
WallochMatt <matthewrwalloch@gmail.com>
2022-09-06 11:28:34 -0500

    feat: Completed second task

--- algorithms.py
+++ algorithms.py
@@ -11,5 +11,26 @@ def is_it_even(number):
         return True
     else:
         return False
+#print(is_it_even())
 
-print(is_it_even())
+
+#Less than 100
+
+
+#Linear O(n)
+def is_less_100(list_of_numbers):
+    """
+    Parameter:
+    list_of_numbers : [int] -> The list of ints to check if less than 100
+
+    Returns
+    bool -> True if all are less, False if any are over 100
+    """
+    for number in list_of_numbers:
+        if number >= 100:
+            return False
+    else:
+        return True
+
+print(is_less_100([]))#input a list of numbers to this argument
+
